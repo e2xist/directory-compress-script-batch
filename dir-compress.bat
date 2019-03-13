6c9d25813ee187556b850f26cb781020daf6cd52
exizt <e2xist@gmail.com>
2019-03-14 02:59:47 +0900

    Update dir-compress.bat

--- dir-compress.bat
+++ dir-compress.bat
@@ -2,29 +2,29 @@
 MODE CON COLS=60 LINES=11
 COLOR 3F
 
-REM It requires the 7z(or bandizip)
+REM 7z 을 필요로 합니다.
 
 ECHO ======================================
-ECHO Folder Compression Process
-ECHO Author : e2xist
+ECHO 폴더 압축 백업 배치 파일
+ECHO Author : e2xist (e2xist.tistory.com)
 ECHO ======================================
 
 
-REM From folder path
+REM 압축할 경로를 지정해줍니다.
 SET _MY_FOLDER=d:\test\abcd
-REM to folder path
+REM 압축한 파일을 저장할 경로를 지정해줍니다.
 SET _TO_FOLDER=d:\test
-REM compress file name 
+REM 압축파일명을 지정합니다. (이 파일 뒤로 날짜가 적히게 됩니다)
 SET FNAME=filename123
 
-REM get _MY_DATETIME
+REM 날짜시간 얻어오기
 CALL :SETDATETIME
 SET FNAME="%FNAME%.%_MY_DATETIME%.zip"
 
-REM 7z compress
+REM 7z 으로 압축
 7z a %_TO_FOLDER%\%FNAME% %_MY_FOLDER%
 
-ECHO The proccess has been completed.
+ECHO 완료되었습니다.
 TIMEOUT /t 10
 
 
@@ -32,19 +32,20 @@ REM close
 GOTO :EOF
 
 REM =============================
-REM functional
+REM 함수 구현 부분
 :SETDATETIME
+REM 결과 -> 2015-03-12
 SET _MY_DATE=%DATE%
-REM  result ->2015-03-12
+REM 결과 -> 20150312
 SET _MY_DATE=%_MY_DATE:-=%
-REM  result ->20150312
+REM 결과 -> 1:40:03.01
 SET _MY_TIME=%TIME%
-REM  result -> 1:40:03.01
+REM 결과 ->  01:40:03.01
 SET _MY_TIME=%_MY_TIME: =0%
-REM  result ->  01:40:03.01
+REM 결과 ->  01:40:03
 SET _MY_TIME=%_MY_TIME:~0,-3%
-REM  result ->  01:40:03
+REM 결과 ->  014003
 SET _MY_TIME=%_MY_TIME::=%
-REM  result ->  014003
+REM 최종 결과 -> 20150312_014003
 SET _MY_DATETIME=%_MY_DATE%_%_MY_TIME%
 REM =============================
